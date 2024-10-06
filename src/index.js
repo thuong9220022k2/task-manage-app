94952a86c2a0e0b4917ab8f80af23979b7dbd319
thuong <thuong9220022k2@gmail.com>
2024-10-06 10:36:07 +0700

    fix redirect page to task page

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 // import { HashRouter, Route, Switch, Redirect } from 'react-router-dom';
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import { ChakraProvider } from '@chakra-ui/react'
 import { baseTheme } from '@chakra-ui/theme'
 import { Provider } from '@chakra-ui/react/provider'
